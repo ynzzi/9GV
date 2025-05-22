994161c8491d52bdfc8496f185338e8042bb49aa
sujiinnn <sujiinnn@gmail.com>
2025-05-22 12:11:45 +0900

    1st commit

---
+++ googv/src/main/java/com/example/googv/GoogvApplication.java
@@ -0,0 +1,13 @@
+package com.example.googv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogvApplication.class, args);
+	}
+
+}
